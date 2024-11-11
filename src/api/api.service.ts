0a553439799837f1f80c23c544961c28e2fb68c7
agmuraro <arthurmuraro18@gmail.com>
2024-11-10 22:53:43 -0300

    URL container microsserviço

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -7,64 +7,64 @@ export class ApiService {
     constructor(private httpService: HttpService) { }
 
     async createProduct(data: any) {
-        const response = await lastValueFrom(this.httpService.post('URL_MICROSSERVICO/product', data));
+        const response = await lastValueFrom(this.httpService.post('https://microcontainer.livelyforest-7ff538b4.brazilsouth.azurecontainerapps.io/product', data));
         return response.data;
     }
 
     async findProduct(id: string) {
-        const response = await lastValueFrom(this.httpService.get('URL_MICROSSERVICO/product/${id}'));
+        const response = await lastValueFrom(this.httpService.get('https://microcontainer.livelyforest-7ff538b4.brazilsouth.azurecontainerapps.io/product/${id}'));
         return response.data;
     }
 
     async findProducts() {
-        const response = await lastValueFrom(this.httpService.get('URL_MICROSSERVICO/user'));
+        const response = await lastValueFrom(this.httpService.get('https://microcontainer.livelyforest-7ff538b4.brazilsouth.azurecontainerapps.io/user'));
         return response.data;
     }
 
     async updateProduct(userData: any) {
-        const response = await lastValueFrom(this.httpService.put('URL_MICROSSERVICO/product', userData));
+        const response = await lastValueFrom(this.httpService.put('https://microcontainer.livelyforest-7ff538b4.brazilsouth.azurecontainerapps.io/product', userData));
         return response.data;
     }
 
     async deleteProduct(userData: any) {
-        const response = await lastValueFrom(this.httpService.delete('URL_MICROSSERVICO/product', userData));
+        const response = await lastValueFrom(this.httpService.delete('https://microcontainer.livelyforest-7ff538b4.brazilsouth.azurecontainerapps.io/product', userData));
         return response.data;
     }
 
     async createUser(userData: any) {
-        const response = await lastValueFrom(this.httpService.post('URL_MICROSSERVICO/user', userData));
+        const response = await lastValueFrom(this.httpService.post('https://microcontainer.livelyforest-7ff538b4.brazilsouth.azurecontainerapps.io/user', userData));
         return response.data;
     }
 
     async findUsers() {
-        const response = await lastValueFrom(this.httpService.get('URL_MICROSSERVICO/user'));
+        const response = await lastValueFrom(this.httpService.get('https://microcontainer.livelyforest-7ff538b4.brazilsouth.azurecontainerapps.io/user'));
         return response.data;
     }
 
 
 
     async findUser(id: string) {
-        const response = await lastValueFrom(this.httpService.get('URL_MICROSSERVICO/user/${id}'));
+        const response = await lastValueFrom(this.httpService.get('https://microcontainer.livelyforest-7ff538b4.brazilsouth.azurecontainerapps.io/user/${id}'));
         return response.data;
     }
 
     async updateUser(userData: any) {
-        const response = await lastValueFrom(this.httpService.put('URL_MICROSSERVICO/user', userData));
+        const response = await lastValueFrom(this.httpService.put('https://microcontainer.livelyforest-7ff538b4.brazilsouth.azurecontainerapps.io/user', userData));
         return response.data;
     }
 
     async deleteUser(userData: any) {
-        const response = await lastValueFrom(this.httpService.delete('URL_MICROSSERVICO/user', userData));
+        const response = await lastValueFrom(this.httpService.delete('https://microcontainer.livelyforest-7ff538b4.brazilsouth.azurecontainerapps.io/user', userData));
         return response.data;
     }
 
     async createOrder(orderData: any) {
-        const response = await lastValueFrom(this.httpService.post('URL_MICROSSERVICO/order', orderData));
+        const response = await lastValueFrom(this.httpService.post('https://microcontainer.livelyforest-7ff538b4.brazilsouth.azurecontainerapps.io/order', orderData));
         return response.data;
     }
 
     async findOrder(id: string) {
-        const response = await lastValueFrom(this.httpService.get('URL_MICROSSERVICO/order/${id}'));
+        const response = await lastValueFrom(this.httpService.get('https://microcontainer.livelyforest-7ff538b4.brazilsouth.azurecontainerapps.io/order/${id}'));
         return response.data;
     }
 }
